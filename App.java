a083febc7dff6e140124156e34f12fd639746c2f
Fabio Toguchi <fabiodtoguchi@gmail.com>
2023-10-26 05:03:43 -0400

    Commit Work

---
+++ App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
+
+//I tried various approaches to practice as much as possible. Please let me know if they are incorrect or if there's room for improvement.
+
+
+/*public class App {
+    public static void main (String[] args) {
+        for (int count = 0; count < 5; count = count + 1) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}*/
+
+/*public class App {
+    public static void main (String[] args) {
+        int counter = 0;
+        while (counter < 5) {
+            counter = counter + 1;
+            System.out.println("Welcome to Java");
+        }
+    }
+}*/
+
+/*public class App {
+    public static void main(String[] args) {
+        int count = 0;
+        while(true){
+            count = count + 1;
+            System.out.println("Welcome to Java");
+            if (count >= 5) {
+                break;
+            }
+        }
+    }
+}*/
